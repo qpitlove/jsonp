d4941b35c7fa1c98c3451982ef94c120f9097ebe
김정기 <kim.jeongki80@navercorp.com>
2024-04-08 16:24:27 +0900

    feat: add error handler

--- index.js
+++ index.js
@@ -91,6 +91,9 @@ function jsonp(url, opts, fn){
   // create script
   script = document.createElement('script');
   script.src = url;
+  script.addEventListener('error',function(error){
+    window[id](error);
+  });
   target.parentNode.insertBefore(script, target);
 
   return cancel;
